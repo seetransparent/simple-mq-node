892663059f771412a6251190bec7024aa2b8e712
Nil Sanz <nsblenin@gmail.com>
2018-06-06 12:03:40 +0200

    TD-311 Prepare git hooks

---
+++ git-hooks/commit-msg.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const assert = require('assert');
+
+const fileCommitMessage = process.argv[2];
+assert(fileCommitMessage, 'Commit message file not found');
+const commitMessage = fs.readFileSync(fileCommitMessage, 'utf8');
+assert(commitMessage, 'Commit message not found');
+if (/^(HOTFIX|[A-Z]{1,6}-[0-9]{1,6})/.test(commitMessage)) {
+  process.exit(0);
+} else {
+  console.log(`Invalid commit message <${commitMessage.trim()}>. Please remember to add the issue number or HOTFIX if none`);
+  process.exit(1);
+}
