5f83c994b2ff0f810e59ac45144bcee39402efc2
Felipe <ergoithz@gmail.com>
2019-02-26 15:49:38 +0100

    TD-589 add missing module

---
+++ src/amqp/utils.ts
@@ -0,0 +1,34 @@
+import * as url from 'url';
+import { Options } from 'amqplib';
+
+import { resolveHost } from '../utils';
+
+/**
+ * Resolves hostname from connection string or object avoiding
+ * DNS issues caused by https://github.com/nodejs/node/issues/21309
+ *
+ * @param connection amqplib connection string or object
+ */
+export async function resolveConnection(
+  connection: string | Options.Connect,
+): Promise<Options.Connect> {
+  if (typeof connection === 'string') {
+    const parts = url.parse(connection, true);
+    const [, username, password]: (string | undefined)[] = (
+      /^([^:]+)(?:\:(.*))?$/.exec(parts.auth || '')
+      || []
+    );
+    return {
+      username,
+      password,
+      protocol: `${parts.protocol || 'amqp'}:`,
+      hostname: await resolveHost(parts.hostname || 'localhost'),
+      port: parseInt(parts.port || '0', 10) || undefined,
+      vhost: parts.pathname ? parts.pathname.substr(1) : undefined,
+    };
+  }
+  return {
+    ...connection,
+    hostname: await resolveHost(connection.hostname || 'localhost'),
+  };
+}
